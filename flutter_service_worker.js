a01c96b66703f2a1d75c3e0e8c91982f292e8fb0
Valentin.E <98782476+ValentinEremia@users.noreply.github.com>
2022-10-07 08:26:17 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "2cedf0a77b62b2a1c8eb4dad7378713c",
 "icons/Icon-maskable-192.png": "a08ff5e5c48c597a4b3db79ac1e9fa06",
 "icons/Icon-maskable-512.png": "2cedf0a77b62b2a1c8eb4dad7378713c",
-"index.html": "06cd2a4b6890ac1c046e5fd7452a576c",
-"/": "06cd2a4b6890ac1c046e5fd7452a576c",
-"main.dart.js": "e98754fddfe1542b5e56e50c6c9c3520",
+"index.html": "12322bd858ba3cd7245b55109a14b564",
+"/": "12322bd858ba3cd7245b55109a14b564",
+"main.dart.js": "4622f9602895a76419982f9d18276742",
 "manifest.json": "d8d5bfc70a9647f7b107e45f58fb0a67",
 "version.json": "2f6248792f3eff5965bd26491dd93b4b"
 };
